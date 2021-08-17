8084e11d2384b372b11de3492788e913cf965dcc
ASK28 <ASK28@users.noreply.github.com>
2021-08-17 09:19:44 +0800

    Safety node code
    AEB still not working

--- akshay021_lab2/scripts/akshay021_safety.py
+++ akshay021_lab2/scripts/akshay021_safety.py
@@ -1,40 +1,57 @@
 #!/usr/bin/env python
 import rospy
+import numpy as np
 
-# TODO: import ROS msg types and libraries
+from ackermann_msgs.msg import AckermannDriveStamped
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Bool
+from nav_msgs.msg import Odometry
+
+mag_rel_vel = dir_rel_vel = 0
+ttc = 999999
+
+def odom_callback(odom_msg):
+    global mag_rel_vel, dir_rel_vel
 
 
-class Safety(object):
-    """
-    The class that handles emergency braking.
-    """
+    w = odom_msg.pose.pose.orientation.w
+    mag_rel_vel = odom_msg.twist.twist.linear.x
+    dir_rel_vel = 2*np.arccos(w)
 
-    def __init__(self):
-        """
-        One publisher should publish to the /brake topic with a AckermannDriveStamped brake message.
-        One publisher should publish to the /brake_bool topic with a Bool message.
-        You should also subscribe to the /scan topic to get the LaserScan messages and
-        the /odom topic to get the current speed of the vehicle.
-        The subscribers should use the provided odom_callback and scan_callback as callback methods
-        NOTE that the x component of the linear velocity in odom is the speed
-        """
-        self.speed = 0
-        # TODO: create ROS subscribers and publishers.
+def scan_callback(scan_msg):
+    global ttc
 
-    def odom_callback(self, odom_msg):
-        # TODO: update current speed
-        self.speed = 0
+    # TODO: calculate TTC
+    n = int((scan_msg.angle_max-scan_msg.angle_min)/scan_msg.angle_increment) + 1
 
-    def scan_callback(self, scan_msg):
-        # TODO: calculate TTC
+    rdot = [(mag_rel_vel*np.cos(0-(i*scan_msg.angle_increment))) for i in range(-n/2,n/2)]
+    rdot = np.clip(rdot, 0, None)
 
-        # TODO: publish brake message and publish controller bool
+    ttc = np.divide(scan_msg.ranges, rdot)
+    pubs()
+
+    # TODO: publish brake message and publish controller bool
+    
+def pubs(): 
+    pub1 = rospy.Publisher('brake', AckermannDriveStamped, queue_size=1)
+    pub2 = rospy.Publisher('brake_bool', Bool, queue_size=1)
+    
+    print(0 < np.min(ttc) < 5)
+    if 0 < np.min(ttc) < 5:
+        ack_msg = AckermannDriveStamped()
+        ack_msg.drive.speed = 0
+        
+        pub1.publish(ack_msg)
+        pub2.publish(True)
+    else:
         pass
-
+        
 def main():
     rospy.init_node('safety_node')
-    sn = Safety()
+    sub1 = rospy.Subscriber('odom', Odometry, odom_callback)
+    sub2 = rospy.Subscriber('scan', LaserScan, scan_callback)
     rospy.spin()
+    
 
 
 if __name__ == '__main__':
