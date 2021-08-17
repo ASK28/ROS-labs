c1906aefdf36876edcbab8c59ed82c2937718953
ASK28 <ASK28@users.noreply.github.com>
2021-08-17 14:07:52 +0800

    Updated safety node script
    Converted code to classes and objects.

--- akshay021_lab2/scripts/akshay021_safety.py
+++ akshay021_lab2/scripts/akshay021_safety.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import rospy
 import numpy as np
 
@@ -7,54 +8,50 @@ from nav_msgs.msg import Odometry
 from std_msgs.msg import Bool
 from ackermann_msgs.msg import AckermannDriveStamped
 
-vel = 0
-ttc = 999999
+class Safety(object):
+    #The class that handles emergency braking.
 
-def odom_callback(odom_msg):
-    global vel
+    def __init__(self):
+        #Publisher for /brake
+        self.pub1 = rospy.Publisher('brake', AckermannDriveStamped, queue_size=1)
+        #Publisher for /brake_bool
+        self.pub2 = rospy.Publisher('brake_bool', Bool, queue_size=1)
+        #Subscribers to /odom and /scan
+        self.sub1 = rospy.Subscriber('odom', Odometry, self.odom_callback, queue_size=1)
+        self.sub2 = rospy.Subscriber('scan', LaserScan, self.scan_callback, queue_size=1)
 
-    vel = odom_msg.twist.twist.linear.x
+    def odom_callback(self, odom_msg):
+        #Get latest speed/velocity
+        self.vel = odom_msg.twist.twist.linear.x
 
-def scan_callback(scan_msg):
-    global ttc
-    # TODO: calculate TTC
-    n = int((scan_msg.angle_max-scan_msg.angle_min)/scan_msg.angle_increment) + 1
+    def scan_callback(self, scan_msg):
+        #Calculate number of laser datapoints
+        n = len(scan_msg.ranges)
+        #Create an array of all the beam angles
+        angles = np.linspace(scan_msg.angle_min, scan_msg.angle_max, n) 
 
-    rdot = [(vel*np.cos(-(i*scan_msg.angle_increment))) for i in range(-n/2,n/2)]
-    rdot = np.clip(rdot, 0, None)
+        #Calculating rdot
+        rdot = [self.vel*np.cos(i) for i in angles]
+        rdot = np.clip(rdot, 0, None)
 
-    ttc = np.divide(scan_msg.ranges, rdot)
+        #Calculating ttc
+        ttc = np.divide(scan_msg.ranges, rdot)
 
-    # TODO: publish brake message and publish controller bool
-    
-def pubs():
-    pub1 = rospy.Publisher('brake', AckermannDriveStamped, queue_size=1)
-    pub2 = rospy.Publisher('brake_bool', Bool, queue_size=1)
-    rate = rospy.Rate(700)
-    
-    
-
-    while not rospy.is_shutdown():
+        #------------------------------------------------------
+        # Checking ttc and braking if necessary
         if 0 < np.min(ttc) < 0.4:
-            print(np.min(ttc))
-            ack_msg = AckermannDriveStamped()
-            ack_msg.drive.speed = 0
-            
-            pub1.publish(ack_msg)
-            pub2.publish(True)
-        else:
-            rate.sleep()
-        
+            brake_msg = AckermannDriveStamped()
+            brake_msg.drive.speed = 0
+
+            self.pub1.publish(brake_msg)
+            self.pub2.publish(True)
+
+
+
 def main():
     rospy.init_node('safety_node')
-
-    try:
-        sub1 = rospy.Subscriber('odom', Odometry, odom_callback, queue_size=1)
-        sub2 = rospy.Subscriber('scan', LaserScan, scan_callback, queue_size=1)
-        pubs()
-    except rospy.ROSInterruptException:
-        pass
-    
+    sn = Safety()
+    rospy.spin()
 
 
 if __name__ == '__main__':
