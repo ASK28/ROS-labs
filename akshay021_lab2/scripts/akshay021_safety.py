345c43068ef11a74089b54dd6f5ad5bcaadb0572
ASK28 <ASK28@users.noreply.github.com>
2021-08-17 13:23:05 +0800

    Updated safety node
    Safety node working!

--- akshay021_lab2/scripts/akshay021_safety.py
+++ akshay021_lab2/scripts/akshay021_safety.py
@@ -2,55 +2,58 @@
 import rospy
 import numpy as np
 
-from ackermann_msgs.msg import AckermannDriveStamped
 from sensor_msgs.msg import LaserScan
-from std_msgs.msg import Bool
 from nav_msgs.msg import Odometry
+from std_msgs.msg import Bool
+from ackermann_msgs.msg import AckermannDriveStamped
 
-mag_rel_vel = dir_rel_vel = 0
+vel = 0
 ttc = 999999
 
 def odom_callback(odom_msg):
-    global mag_rel_vel, dir_rel_vel
+    global vel
 
-
-    w = odom_msg.pose.pose.orientation.w
-    mag_rel_vel = odom_msg.twist.twist.linear.x
-    dir_rel_vel = 2*np.arccos(w)
+    vel = odom_msg.twist.twist.linear.x
 
 def scan_callback(scan_msg):
     global ttc
-
     # TODO: calculate TTC
     n = int((scan_msg.angle_max-scan_msg.angle_min)/scan_msg.angle_increment) + 1
 
-    rdot = [(mag_rel_vel*np.cos(0-(i*scan_msg.angle_increment))) for i in range(-n/2,n/2)]
+    rdot = [(vel*np.cos(-(i*scan_msg.angle_increment))) for i in range(-n/2,n/2)]
     rdot = np.clip(rdot, 0, None)
 
     ttc = np.divide(scan_msg.ranges, rdot)
-    pubs()
 
     # TODO: publish brake message and publish controller bool
     
-def pubs(): 
+def pubs():
     pub1 = rospy.Publisher('brake', AckermannDriveStamped, queue_size=1)
     pub2 = rospy.Publisher('brake_bool', Bool, queue_size=1)
+    rate = rospy.Rate(700)
     
-    print(0 < np.min(ttc) < 5)
-    if 0 < np.min(ttc) < 5:
-        ack_msg = AckermannDriveStamped()
-        ack_msg.drive.speed = 0
-        
-        pub1.publish(ack_msg)
-        pub2.publish(True)
-    else:
-        pass
+    
+
+    while not rospy.is_shutdown():
+        if 0 < np.min(ttc) < 0.4:
+            print(np.min(ttc))
+            ack_msg = AckermannDriveStamped()
+            ack_msg.drive.speed = 0
+            
+            pub1.publish(ack_msg)
+            pub2.publish(True)
+        else:
+            rate.sleep()
         
 def main():
     rospy.init_node('safety_node')
-    sub1 = rospy.Subscriber('odom', Odometry, odom_callback)
-    sub2 = rospy.Subscriber('scan', LaserScan, scan_callback)
-    rospy.spin()
+
+    try:
+        sub1 = rospy.Subscriber('odom', Odometry, odom_callback, queue_size=1)
+        sub2 = rospy.Subscriber('scan', LaserScan, scan_callback, queue_size=1)
+        pubs()
+    except rospy.ROSInterruptException:
+        pass
     
 
 
